02f0bc593d7bb25fe68d1f274d30a35307ccb807
Tarso Queiroz <tarsoqueiroz@gmail.com>
2017-07-26 16:17:32 -0300

    Ethereum environment

---
+++ Environments/Ethereum/ether-0ro.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ETHER0ROVOL="/home/tarso/Environments/Ethereum/Ether-0ro"
+echo $ETHER0ROVOL
+mkdir -p $ETHER0ROVOL
+
+docker run --name    ether0ro \
+           -v        $ETHER0ROVOL:/root/.ethereum \
+           -p        30303:30303 \
+           -p        28015:28015 \
+           -p        8545:8545 \
+           -d        ethereum/client-go
