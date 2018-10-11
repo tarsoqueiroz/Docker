a7922272eabfd0308ac1c2f0fb1684602be1317a
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-10-11 16:35:07 -0300

    Adjusts on Ethereum environment builder

--- Environments/Ethereum/ether-0ro.sh
+++ Environments/Ethereum/ether-0ro.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
-ETHER0ROVOL="/home/tarso/Environments/Ethereum/Ether-0ro"
+#ETHER0ROVOL="/home/tarso/Environments/Ethereum/Ether-0ro"
+ETHER0ROVOL="/Data/Environment/Ethereum/Ether-0ro"
 echo $ETHER0ROVOL
 mkdir -p $ETHER0ROVOL
 
-docker run --name    ether0ro                     \
+docker run -it                                    \
+           --name    ethereum                     \
            -v        $ETHER0ROVOL:/root/.ethereum \
-           -p        30303:30303                  \
-           -p        28015:28015                  \
            -p        8545:8545                    \
+           -p        8546:8546                    \
+           -p        28015:28015                  \
+           -p        30303:30303                  \
+                     ethereum/client-go           \
+           --ws                                   \
            --rpc                                  \
-           --rpcaddr "0.0.0.0"                    \
-           -d        ethereum/client-go
+           --rpcaddr "0.0.0.0"                    
+#           --wsaddr  "0.0.0.0"                    
 
-
-docker run -it 
-           --name    ethereum 
-           -v        /home/tarso/Environments/Ethereum/Ethereum:/root/.ethereum
-           -p        30303:30303 
-           -p        8545:8545
-           -p        8546:8546 
-           ethereum/client-go
-           --ws      
-           --rpc 
-           --rpcaddr "0.0.0.0" 
-           --wsaddr  "0.0.0.0"
-
-
-docker run --name ether0ro -v $ETHER0ROVOL:/root/.ethereum -p 30303:30303 -p 28015:28015 -p 8545:8545 --rpc --rpcaddr "0.0.0.0" -d ethereum/client-go -it
-
-docker run -it --name ethereum -v /home/tarso/Environments/Ethereum/Ethereum:/root/.ethereum -p 30303:30303 -p 8545:8545 -p 8546:8546 ethereum/client-go --ws --rpc --rpcaddr "0.0.0.0" --wsaddr "0.0.0.0"
